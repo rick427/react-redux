02dfb30edc2531cff7f93a2899e5ccf035b561a5
rick427 <rchard@yahoo.com>
2020-03-01 14:07:33 +0100

    setting up actions and reducers

---
+++ src/components/CakeComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const CakeComponent = () => {
+    return (
+        <div className="cake-wrapper">
+            <h2>Number of Cakes</h2>
+            <button>buy cake</button>
+        </div>
+    )
+}
+
+export default CakeComponent
